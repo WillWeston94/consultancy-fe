c13ca132334533711595c5cf573ddc32251338a2
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-11-06 00:29:14 -0700

    add devise views for override purposes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,7 +25,9 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-  config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET']
+  config.omniauth :google_oauth2, 
+                  Rails.application.credentials.google[:google_oauth_client_id], 
+                  Rails.application.credentials.google[:google_oauth_client_secret]
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
