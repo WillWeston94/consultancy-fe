14997265b00806094f814eaf3bcd08ca0718179d
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-11-06 14:31:24 -0700

    update validations for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "validations" do
     it {should validate_presence_of(:email)}
-    it {should validate_uniqueness_of(:email)}
+    it {should validate_uniqueness_of(:email).case_insensitive}
     it {should validate_presence_of(:password)}
-    it {should have_secure_password}
   end
 end
